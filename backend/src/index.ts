6fefee9c7b1f3b4e5098aa052cfdb92d9e3f0468
Gilma <gilma@gmail.com>
2026-08-14 23:38:48 -0500

    feat: configurar estructura inicial y servidor base con Express

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para leer JSON
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'API de Inventario de Equipos funcionando correctamente 🚀' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});

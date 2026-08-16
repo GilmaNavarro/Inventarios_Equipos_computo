80f4075a6c09770348fb3656d3ed239e6d4c3ecf
Gilma <gilma@gmail.com>
2026-08-15 23:06:05 -0500

    feat: crear modelo Equipment, configurar CORS y sincronizar base de datos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,43 @@
-import 'dotenv/config';
-import express, { type Request, type Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
+import cors from 'cors';
+import db from './config/database.js'; // Tu puente a la base de dato
+import Equipment from './models/Equipment.js'; // Tu nuevo modelo (el molde)
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// Middleware para leer JSON
+app.use(cors());
 app.use(express.json());
 
-// Ruta de prueba
+// Tu ruta de prueba
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'API de Inventario de Equipos funcionando correctamente 🚀' });
+  res.send('¡El servidor del Inventario está funcionando perfectamente!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// --- AQUÍ ES DONDE VA LA FUNCIÓN ---
+const arrancarServidor = async () => {
+  try {
+    // 1. Probamos que el puente funciona
+    await db.authenticate();
+    console.log('Conexión a la base de datos establecida con éxito.');
+
+    // 2. Le decimos a Sequelize que construya la tabla de Equipos (si no existe)
+    await db.sync(); 
+    console.log('Modelos sincronizados con la base de datos.');
+
+    // 3. Encendemos el servidor para recibir usuarios
+    app.listen(port, () => {
+      console.log(` Servidor corriendo en http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+  }
+};
+
+// Y finalmente, ejecutamos la función
+arrancarServidor();
